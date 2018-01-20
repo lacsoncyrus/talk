f489b0b70e944df1e1c4a6079f9da77984866105
Cyrus Lacson <lacsoncyrus@gmail.com>
2018-01-20 15:31:15 +0800

    ADDED IF STATEMENT

--- sample.php
+++ sample.php
@@ -5,5 +5,9 @@
  <body>
  <?php echo '<p>I knoe de wey</p><br>'; ?> 
  <?php echo '<p>to de kween.</p>'; ?>
+ <?php echo '<p>Glory to uganda.</p>'; ?>
+    if(dasdasdsad){
+        code here
+    }
  </body>
 </html>
